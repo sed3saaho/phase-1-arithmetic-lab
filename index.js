f65e508e2def1659df3b962b1f03d54450aa44ec
Sedly <sedly.keith@student.moringaschool.com>
2024-04-11 09:43:18 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+n
+let num1 = 31;
+let num2 = 2;
+let num3 = 20;
+let num4 = 16;
+
+
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+
+let mod = num3 % num4;
+
+
+let max_num = Math.max(5, 10, 20, 15);
